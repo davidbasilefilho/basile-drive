1dd3b0f81a394a93a4aaffe4d5bed3a5d34027cc
David Basile Filho <davidbasilefilho@gmail.com>
2023-09-17 14:44:11 -0300

    disabled firebase deploying, i'm going to set up again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    assetPrefix: ".",
+    target: "serverless",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
